a0e3607c630ac640118fd7586f70a65113fdf5cb
rita <kajirita2002@gmail.com>
2021-04-06 15:12:15 +0900

    made address configurable

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 	"path/filepath"
@@ -9,19 +10,23 @@ import (
 )
 
 type templateHandler struct {
-	once sync.Once
+	once     sync.Once
 	filename string
-	templ *template.Template
+	templ    *template.Template
 }
 
 func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.templ.Execute(w, nil)
+	t.templ.Execute(w, r)
 }
 
 func main() {
+	var addr = flag.String("addr", ":8080", "The address of the application")
+
+	// parse the flags
+	flag.Parse()
 	r := newRoom()
 	http.Handle("/", &templateHandler{filename: "chat.html"})
 
@@ -29,7 +34,8 @@ func main() {
 
 	// get the room going
 	go r.run()
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	log.Println("Starting web server on: ", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
 }
